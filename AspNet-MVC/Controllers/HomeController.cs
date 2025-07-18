18f312dc28477d8736041c981cc0fd4f977746b0
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-18 11:47:49 -0300

    Add project files.

---
+++ AspNet-MVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using AspNet_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
