857ec01a70cdfce2becee12d40c6298f5fafdf10
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-23 11:42:31 -0300

    Criando as controllers

--- AspNet-MVC/Controllers/HomeController.cs
+++ AspNet-MVC/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace AspNet_MVC.Controllers
 
         public IActionResult Index()
         {
-            return View();
+          return View();
         }
 
         public IActionResult Privacy()
